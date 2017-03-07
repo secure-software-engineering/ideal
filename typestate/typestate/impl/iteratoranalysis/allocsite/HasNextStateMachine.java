1d001979d71a1afc9f97ff621687aec4df000cb0
johannes.spaeth <joh.spaeth@googlemail.com>
2017-03-07 15:41:13 +0100

    Implemented analyses as it is done in TSfink: The seeds are the allocation sites.

--- typestate/typestate/impl/iteratoranalysis/allocsite/HasNextStateMachine.java
+++ typestate/typestate/impl/iteratoranalysis/allocsite/HasNextStateMachine.java
@@ -31,118 +31,112 @@ import typestate.finiteautomata.Transition;
 
 public class HasNextStateMachine extends MatcherStateMachine implements TypestateChangeFunction {
 
-  private MatcherTransition initialTrans;
-  private InfoflowCFG icfg;
-  private Set<SootMethod> hasNextMethods;
+	private MatcherTransition initialTrans;
+	private InfoflowCFG icfg;
+	private Set<SootMethod> hasNextMethods;
 
-  public static enum States implements State {
-    NONE, INIT, HASNEXT, ERROR;
+	public static enum States implements State {
+		NONE, INIT, HASNEXT, ERROR;
 
-    @Override
-    public boolean isErrorState() {
-      return this == ERROR;
-    }
+		@Override
+		public boolean isErrorState() {
+			return this == ERROR;
+		}
 
-    @Override
-    public boolean isInitialState() {
-      return this == INIT;
-    }
-  }
+		@Override
+		public boolean isInitialState() {
+			return this == INIT;
+		}
+	}
 
-  HasNextStateMachine(InfoflowCFG cfg) {
-    this.icfg = cfg;
-    initialTrans =
-        new MatcherTransition(States.NONE, retrieveIteratorConstructors(),Parameter.This, States.INIT, Type.None);
-    addTransition(initialTrans);
-    addTransition(
-        new MatcherTransition(States.INIT, retrieveNextMethods(),Parameter.This, States.ERROR, Type.OnReturn));
-    addTransition(
-        new MatcherTransition(States.ERROR, retrieveNextMethods(),Parameter.This, States.ERROR, Type.OnReturn));
-    addTransition(
-        new MatcherTransition(States.HASNEXT, retrieveNextMethods(),Parameter.This, States.INIT, Type.OnReturn));
-    addTransition(new MatcherTransition(States.INIT, retrieveHasNextMethods(),Parameter.This, States.HASNEXT,
-        Type.OnReturn));
-    addTransition(new MatcherTransition(States.HASNEXT, retrieveHasNextMethods(),Parameter.This, States.HASNEXT,
-        Type.OnReturn));
-    addTransition(
-        new MatcherTransition(States.ERROR, retrieveHasNextMethods(),Parameter.This, States.ERROR, Type.OnReturn));
-  }
+	HasNextStateMachine(InfoflowCFG cfg) {
+		this.icfg = cfg;
+		initialTrans = new MatcherTransition(States.NONE, retrieveIteratorConstructors(), Parameter.This, States.INIT,
+				Type.None);
+		addTransition(initialTrans);
+		addTransition(
+				new MatcherTransition(States.INIT, retrieveNextMethods(), Parameter.This, States.ERROR, Type.OnReturn));
+		addTransition(new MatcherTransition(States.ERROR, retrieveNextMethods(), Parameter.This, States.ERROR,
+				Type.OnReturn));
+		addTransition(new MatcherTransition(States.HASNEXT, retrieveNextMethods(), Parameter.This, States.INIT,
+				Type.OnReturn));
+		addTransition(new MatcherTransition(States.INIT, retrieveHasNextMethods(), Parameter.This, States.HASNEXT,
+				Type.OnReturn));
+		addTransition(new MatcherTransition(States.HASNEXT, retrieveHasNextMethods(), Parameter.This, States.HASNEXT,
+				Type.OnReturn));
+		addTransition(new MatcherTransition(States.ERROR, retrieveHasNextMethods(), Parameter.This, States.ERROR,
+				Type.OnReturn));
+	}
 
-  private Set<SootMethod> retrieveHasNextMethods() {
-    if (hasNextMethods == null)
-      hasNextMethods =
-          selectMethodByName(getImplementersOfIterator("java.util.Iterator"), "hasNext");
-    return hasNextMethods;
-  }
+	private Set<SootMethod> retrieveHasNextMethods() {
+		if (hasNextMethods == null)
+			hasNextMethods = selectMethodByName(getImplementersOfIterator("java.util.Iterator"), "hasNext");
+		return hasNextMethods;
+	}
 
-  private Set<SootMethod> retrieveNextMethods() {
-    return selectMethodByName(getImplementersOfIterator("java.util.Iterator"), "next");
-  }
+	private Set<SootMethod> retrieveNextMethods() {
+		return selectMethodByName(getImplementersOfIterator("java.util.Iterator"), "next");
+	}
 
-  private Set<SootMethod> retrieveIteratorConstructors() {
-    Set<SootMethod> selectMethodByName = selectMethodByName(Scene.v().getClasses(), "iterator");
-    Set<SootMethod> res = new HashSet<>();
-    for (SootMethod m : selectMethodByName) {
-      if (m.getReturnType() instanceof RefType) {
-        RefType refType = (RefType) m.getReturnType();
-        SootClass classs = refType.getSootClass();
-        if (classs.equals(Scene.v().getSootClass("java.util.Iterator"))
-            || Scene.v().getActiveHierarchy()
-                .getImplementersOf(Scene.v().getSootClass("java.util.Iterator")).contains(classs)) {
-          res.add(m);
-        }
-      }
-    }
-    return res;
-  }
+	private Set<SootMethod> retrieveIteratorConstructors() {
+		Set<SootMethod> selectMethodByName = selectMethodByName(Scene.v().getClasses(), "iterator");
+		Set<SootMethod> res = new HashSet<>();
+		for (SootMethod m : selectMethodByName) {
+			if (m.getReturnType() instanceof RefType) {
+				RefType refType = (RefType) m.getReturnType();
+				SootClass classs = refType.getSootClass();
+				if (classs.equals(Scene.v().getSootClass("java.util.Iterator")) || Scene.v().getActiveHierarchy()
+						.getImplementersOf(Scene.v().getSootClass("java.util.Iterator")).contains(classs)) {
+					res.add(m);
+				}
+			}
+		}
+		return res;
+	}
 
+	private List<SootClass> getImplementersOfIterator(String className) {
+		SootClass sootClass = Scene.v().getSootClass(className);
+		List<SootClass> list = Scene.v().getActiveHierarchy().getImplementersOf(sootClass);
+		List<SootClass> res = new LinkedList<>();
+		for (SootClass c : list) {
+			res.add(c);
+		}
+		return res;
+	}
 
-  private List<SootClass> getImplementersOfIterator(String className) {
-    SootClass sootClass = Scene.v().getSootClass(className);
-    List<SootClass> list = Scene.v().getActiveHierarchy().getImplementersOf(sootClass);
-    List<SootClass> res = new LinkedList<>();
-    for (SootClass c : list) {
-      res.add(c);
-    }
-    return res;
-  }
+	@Override
+	public Collection<Pair<AccessGraph, EdgeFunction<TypestateDomainValue>>> generate(SootMethod method, Unit unit,
+			Collection<SootMethod> calledMethod) {
+		if (!method.getDeclaringClass().isApplicationClass())
+			return Collections.emptySet();
+		if (unit instanceof AssignStmt) {
+			if (((AssignStmt) unit).getRightOp() instanceof NewExpr) {
+				NewExpr newExpr = (NewExpr) ((AssignStmt) unit).getRightOp();
+				if (newExpr.getType() instanceof RefType) {
+					RefType refType = (RefType) newExpr.getType();
+					if (getImplementersOfIterator("java.util.Iterator").contains(refType.getSootClass())) {
+						Set<Pair<AccessGraph, EdgeFunction<TypestateDomainValue>>> out = new HashSet<>();
+						AssignStmt stmt = (AssignStmt) unit;
+						out.add(new Pair<AccessGraph, EdgeFunction<TypestateDomainValue>>(
+								new AccessGraph((Local) stmt.getLeftOp(), stmt.getLeftOp().getType()),
+								new TransitionFunction(initialTrans)));
+						return out;
+					}
+				}
+			}
 
-  @Override
-  public Collection<Pair<AccessGraph, EdgeFunction<TypestateDomainValue>>> generate(SootMethod method, Unit unit,
-      Collection<SootMethod> calledMethod) {
-    if(!method.getDeclaringClass().isApplicationClass())
-    	return Collections.emptySet();
-    if (unit instanceof AssignStmt) {
-      if (((AssignStmt) unit).getRightOp() instanceof NewExpr) {
-        NewExpr newExpr = (NewExpr) ((AssignStmt) unit).getRightOp();
-        if (newExpr.getType() instanceof RefType) {
-          RefType refType = (RefType) newExpr.getType();
-          if (getImplementersOfIterator("java.util.Iterator").contains(refType.getSootClass())) {
-            Set<Pair<AccessGraph, EdgeFunction<TypestateDomainValue>>> out = new HashSet<>();
-            AssignStmt stmt = (AssignStmt) unit;
-            out.add(new Pair<AccessGraph, EdgeFunction<TypestateDomainValue>>(
-                new AccessGraph((Local) stmt.getLeftOp(), stmt.getLeftOp().getType()),
-                new TransitionFunction(initialTrans)));
-            return out;
-          }
-        }
-      }
+		}
+		return Collections.emptySet();
+	}
 
-    }
-    return Collections.emptySet();
-  }
+	@Override
+	public Set<Transition> getReturnTransitionsFor(AccessGraph callerD1, Unit callSite, SootMethod calleeMethod,
+			Unit exitStmt, AccessGraph exitNode, Unit returnSite, AccessGraph retNode) {
+		if (retrieveHasNextMethods().contains(calleeMethod)) {
+			if (icfg.getMethodOf(callSite).getSignature().contains("java.lang.Object next()"))
+				return Collections.emptySet();
+		}
 
-
-  @Override
-  public Set<Transition> getReturnTransitionsFor(AccessGraph callerD1, Unit callSite,
-      SootMethod calleeMethod, Unit exitStmt, AccessGraph exitNode, Unit returnSite,
-      AccessGraph retNode) {
-    if (retrieveHasNextMethods().contains(calleeMethod)) {
-      if (icfg.getMethodOf(callSite).getSignature().contains("java.lang.Object next()"))
-        return Collections.emptySet();
-    }
-
-    return super.getReturnTransitionsFor(callerD1, callSite, calleeMethod, exitStmt, exitNode, returnSite, retNode);
-  }
+		return super.getReturnTransitionsFor(callerD1, callSite, calleeMethod, exitStmt, exitNode, returnSite, retNode);
+	}
 }
-
---
+++ typestate/typestate/impl/outputstream/allocsite/OutputStreamAnalysis.java
@@ -0,0 +1,18 @@
+package typestate.impl.outputstream.allocsite;
+
+import ideal.debug.IDebugger;
+import soot.jimple.infoflow.solver.cfg.InfoflowCFG;
+import typestate.TypestateAnalysis;
+import typestate.TypestateDomainValue;
+
+public class OutputStreamAnalysis extends TypestateAnalysis {
+
+  public OutputStreamAnalysis(InfoflowCFG icfg) {
+    super(new OutputStreamStateMachine(icfg), icfg);
+  }
+  
+
+  public OutputStreamAnalysis(InfoflowCFG icfg, IDebugger<TypestateDomainValue> debugger) {
+    super(new OutputStreamStateMachine(icfg), icfg, debugger);
+  }
+}
