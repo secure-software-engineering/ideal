f6f2ceb03ab284b94b217b5db862e5a756c4db43
Johannes Späth <joh.spaeth@googlemail.com>
2017-07-26 18:29:48 +0200

    Do not check for accepting state for static fields

--- src/main/java/ideal/AnalysisSolver.java
+++ src/main/java/ideal/AnalysisSolver.java
@@ -128,7 +128,7 @@ public class AnalysisSolver<V>
 					if (!cell.getRowKey().equals(InternalAnalysisProblem.ZERO)) {
 						continue;
 					}
-					if (BoomerangContext.isParameterOrThisValue(method, cell.getColumnKey().getBase()) || BoomerangContext.isReturnValue(method,cell.getColumnKey().getBase())) {
+					if (cell.getColumnKey().isStatic() || BoomerangContext.isParameterOrThisValue(method, cell.getColumnKey().getBase()) || BoomerangContext.isReturnValue(method,cell.getColumnKey().getBase())) {
 						parameterLocalsAtEndPoint.add(cell.getColumnKey());
 					} else {
 						nonParameterLocalsAtEndPoint.add(cell.getColumnKey());
