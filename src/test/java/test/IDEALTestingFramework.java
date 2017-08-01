6b9c8fb6751213b645a69373196b6dcc76d96712
johannes.spaeth <joh.spaeth@googlemail.com>
2017-08-01 12:50:07 +0200

    Fixed compilation error

--- src/test/java/test/IDEALTestingFramework.java
+++ src/test/java/test/IDEALTestingFramework.java
@@ -95,7 +95,6 @@ public abstract class IDEALTestingFramework extends AbstractTestingFramework{
 				}
 				if (!unsound.isEmpty())
 					throw new RuntimeException("Unsound results: " + unsound);
-				IDEALTestingFramework.this.removeVizFile();
 				if (!imprecise.isEmpty()) {
 					throw new ImprecisionException("Imprecise results: " + imprecise);
 				}
